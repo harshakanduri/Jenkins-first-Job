d2d335617554197014037bbc5ab3aeffda1a5af3
Kanduri Harshavardhan <kanduriharsha8@gmail.com>
2025-11-22 11:58:58 +0530

    Refactor package name and imports in tests

--- src/test/java/com/Jenkins/SpringBootMysqlDockerComposeApplicationTests.java
+++ src/test/java/com/Jenkins/SpringBootMysqlDockerComposeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.telusko;
+package com.jenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootMysqlDockerComposeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
